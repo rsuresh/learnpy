92aaa9c62707e96aa9dbc11f1a2b8c8669dcd1f8
M Rohith Suresh <mrohith.suresh@citrix.com>
2020-05-10 22:26:20 +0530

    first comit : kfold sample.

---
+++ Kfold_attempt.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May  6 19:33:28 2020
+
+@author: mrohiths
+"""
+
+subSet=[]
+dataSet=[]
+startIndex=0
+endIndex=0
+inputDataLength=int(input("Please enter the length of DataSet to geenrate: "))
+fold=int(input("Please enter number of elements one fold can hold: "))
+
+dataSet=[i for i in range(inputDataLength)]
+print(dataSet)
+
+print("")
+
+
+for i in range(0,inputDataLength,fold):
+    if i!=0:
+        endIndex=i+fold
+        if endIndex >= inputDataLength:
+            endIndex=inputDataLength
+        startIndex=i
+        print(f'start index is {startIndex} and end Index is {endIndex}')
+        subSet.append(dataSet[startIndex:endIndex])
+        
+    else:
+        endIndex=fold
+        print(f'start index is {startIndex} and end Index is {endIndex}')
+        print(dataSet[startIndex:endIndex])
+        subSet.append(dataSet[startIndex:endIndex])
+
+print(subSet)
